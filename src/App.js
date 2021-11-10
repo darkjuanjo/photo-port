66b4c27f0fb9b548ff4c97d101e52f71bf385dc4
Juan Jose Paz <juanjosepazroman@hotmail.com>
2021-11-09 23:19:06 -0800

    added useEffect and Gallery

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import './App.css';
-import About from './components/About';
+import React, { useState, useEffect } from 'react';
 import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import { capitalizeFirstLetter } from './utils/helpers';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
-        <About></About>
+        <div>
+          <Gallery></Gallery>
+          <About></About>
+        </div>
       </main>
     </div>
   );
